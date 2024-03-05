75e100e036fe53370b2b52e397ea3afc17e9e1e1
Lurius-Kitsune <LucasFox@outlook.fr>
2024-03-05 14:24:35 +0100

    - Mise a jours du composer

---
+++ src/Entity/Qualite.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\QualiteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: QualiteRepository::class)]
+#[ApiResource]
+class Qualite
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $libelleQualite = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelleQualite(): ?string
+    {
+        return $this->libelleQualite;
+    }
+
+    public function setLibelleQualite(string $libelleQualite): static
+    {
+        $this->libelleQualite = $libelleQualite;
+
+        return $this;
+    }
+}
